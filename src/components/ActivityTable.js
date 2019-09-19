35be97cd723c9e9dc67b12657384fc0c60753242
pai_eayran <eayran@psycharmor.org>
2019-09-19 14:03:24 -0700

    Add dropdown for each row entry for additional content

--- src/components/ActivityTable.js
+++ src/components/ActivityTable.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
 import LoadingOverlay from 'react-loading-overlay';
+import TableEntry from './TableEntry'
 
 class ActivityTable extends Component {
 
     render() {
-        const activities = this.props.activities.map((item, key) =>
-            <li key={key}>Name: {item.first_name} {item.last_name}; course id: {item.activity.id}</li>
-        );
+        const activities = this.props.activities.map((item, key) => {
+            // let completed = "no";
+            // if (item.activity.timestamps_formatted.completed) {
+            //     completed = "yes";
+            // }
+            // return <li key={key}>Name: {item.first_name} {item.last_name}; course id: {item.activity.id} date completed: {completed}</li>
+            return <TableEntry key={key} activity={item}></TableEntry>
+        });
 
         return (
             <div>
-                <LoadingOverlay active={this.props.showOverlay}>
                 <ul>
-                    {activities}
+                    <li className="table-entry">
+                        <div className="table-entry-header label">
+                            <div>Name</div>
+                            <div>Course ID</div>
+                            <div>Completed?</div>
+                        </div>
+                    </li>
+                    <LoadingOverlay active={this.props.showOverlay}>
+                        {activities}
+                    </LoadingOverlay>
                 </ul>
-                </LoadingOverlay>
             </div>
         )
     }
